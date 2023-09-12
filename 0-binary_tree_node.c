58243ec92bc695f6949d52dc31f4fc90b4d105bd
GreyLeaf23 <christian.torres2894@gmail.com>
2023-09-12 18:22:06 -0400

    'if  condition' for Task 0 has been update it.

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,11 +12,18 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 binary_tree_t *new_node; /*Declaration of New Node.*/
 
-new_node = malloc(sizeof(binary_tree_t)); /*Allocate memory*/
+if (parent == NULL || value == '\0') /*Error Check.*/
+{
+   return (NULL);
+}
+
+new_node = malloc(sizeof(binary_tree_t)); /*Allocate memory.*/
 
 if (new_node == NULL) /*Error Check.*/
+{
    return (NULL); 
-   
+}
+
 new_node->n = value; /*Content the node holds.*/
 new_node->parent = parent; /*Birthplace.*/
 new_node->left = NULL; /*No sibling node*/
