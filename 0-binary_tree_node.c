c18881dbc471e1ca042c915975e8586ecd958761
GreyLeaf23 <christian.torres2894@gmail.com>
2023-09-13 10:06:45 -0400

    Betty.

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,21 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * bianry_tree_node - Create new node in binary tree.
+ * binary_tree_node - Create new node in binary tree.
  * @parent: Origin node for creation.
  * @value: Data the node holds.
- * 
+ *
  * Return: New node.
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-   
+
 binary_tree_t *new_node = malloc(sizeof(binary_tree_t)); /*Allocate memory.*/
 
 if (new_node == NULL) /*Error Check.*/
 {
-   return (NULL); 
+   return (NULL);
 }
 
 new_node->n = value; /*Content the node holds.*/
