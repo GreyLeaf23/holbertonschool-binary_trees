a18ae7a23b981b20c6a03841538032d05c821a03
GreyLeaf23 <christian.torres2894@gmail.com>
2023-09-13 15:10:48 -0400

    Hot fix.

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,12 +9,12 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    if (tree == NULL) /*Error check if the tree doesn't exist.*/
-        return (0);
-
     /*Declaration of height variables.*/
     size_t left_height = 0;
     size_t right_height = 0;
+
+    if (tree == NULL) /*Error check if the tree doesn't exist.*/
+        return (0);
 
     if (tree->left != NULL)
         left_height += 1 + binary_tree_height(tree->left); /*Assign left height of tree to variable.*/
