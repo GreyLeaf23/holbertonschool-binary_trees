ce618b0657105f0185385f43a74ee50952069dae
GreyLeaf23 <christian.torres2894@gmail.com>
2023-09-13 18:53:57 -0400

    Modify file.

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,11 +13,14 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
     size_t right_leaves = 0;
 
     if (tree == NULL)
+    {
         return (0);
+    }
 
     if (tree->left == NULL && tree->right == NULL)
+    {
         return (1);
-
+    }
 
 return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
