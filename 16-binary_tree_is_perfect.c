df46784849e9a31f62e553040d5e0adf9c969810
GreyLeaf23 <christian.torres2894@gmail.com>
2023-09-14 15:44:13 -0400

    Implement 'depth' function into Task 16.

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -18,7 +18,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
     if (tree->left == NULL || tree->right == NULL)
         return (0);
 
-    if (tree->left != NULL && tree->right != NULL)
+/*Check formation structure for both height and depth of subtrees.*/
+    if (binary_tree_height(tree->left) == binary_tree_height(tree->right) &&
+            binary_tree_depth(tree->left) == binary_tree_depth(tree->right))
     {
         return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
     }
@@ -54,4 +56,30 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 /*Cool implementaion of if-else on a single line!*/
     return (left_height > right_height ? left_height : right_height);
+}
+
+
+
+/**
+ * binary_tree_depth - measure depth of binary tree.
+ * @tree: pointer to the root of binary tree.
+ *
+ * Return: depth of binary tree.
+ */
+
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+
+size_t depth = 0;
+
+if (tree == NULL)
+    return (0);
+
+if (tree->parent != NULL)
+{
+    depth += 1 + binary_tree_depth(tree->parent); /*Assign variable as depth of binary tree.*/
+}
+
+    return (depth);
 }
