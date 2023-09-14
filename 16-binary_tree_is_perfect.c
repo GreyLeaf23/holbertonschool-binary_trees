4c9441624f109dc8b5d7599be77cbf468a948a0b
GreyLeaf23 <christian.torres2894@gmail.com>
2023-09-14 15:14:14 -0400

    Add 'if' condition.

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -13,9 +13,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
         return (0);
 
     if (tree->left == NULL && tree->right == NULL)
-    {
         return (1);
-    }
+
+    if (tree->left == NULL || tree->right == NULL)
+        return (0);
 
     if (tree->left != NULL && tree->right != NULL)
     {
